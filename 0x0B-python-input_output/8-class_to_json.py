2b0d4fe491df886e690cf1814e1922f833aa3b20
njoroge-s <sarahnjoroge2015@gmail.com>
2022-09-27 03:35:47 -0700

    bug fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-""" Module that returns the dictionary description with a simple
-data structure for a JSON serialization of an object
-"""
+"""Defines a Python class-to-JSON function."""
 
 
 def class_to_json(obj):
-    """ Function that retuns the dictionary description of an obj """
-
-    res = {}
-    if hasattr(obj, "__dict__"):
-        res = obj.__dict__.copy()
-    return res 
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__ 
